bed0546ca5bae536c1678de1bbc1f39b214df6fb
wakashi03 <wakachaso38@gmail.com>
2023-12-10 06:10:23 +0900

    Initial commit after re-running rails new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "goshichi_de_production"
+  # config.active_job.queue_name_prefix = "goshichide_production"
 
   config.action_mailer.perform_caching = false
 
