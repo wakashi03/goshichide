613ea454b77372bda18acbd9ef3f3c0df6342a96
wakashi03 <wakachaso38@gmail.com>
2023-12-11 22:32:05 +0900

    Generator file creation restriction

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module Goshichide
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
