9bf16fc3648de8d12701eee481cbdd3c9cd54335
wakashi03 <wakachaso38@gmail.com>
2024-01-05 20:50:39 +0900

    Add Sensyu model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_29_083717) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_30_153949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "senryus", force: :cascade do |t|
+    t.string "kamigo", null: false
+    t.string "nakashichi", null: false
+    t.string "shimogo", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_senryus_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -24,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_29_083717) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "senryus", "users"
 end
