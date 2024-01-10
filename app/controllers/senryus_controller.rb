346ad90b19f9c28f8641476882f2f315156f6ad6
wakashi03 <wakachaso38@gmail.com>
2024-01-10 19:54:08 +0900

    Add create action for senryu controller

--- app/controllers/senryus_controller.rb
+++ app/controllers/senryus_controller.rb
@@ -4,4 +4,28 @@ class SenryusController < ApplicationController
   def index
     @senryus = Senryu.all.includes(:user).order(created_at: :desc)
   end
+
+  def new
+    @senryus = Senryu.new
+  end
+
+  def create
+    @senryu = current_user.senryus.build(senryu_params)
+    if @senryu.save
+      redirect_to senryus_path, success: t('defaults.message.created', item: Senryu.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_created', item: Senryu.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @senryu = Senryu.find(params[:id])
+  end
+
+  private
+
+  def senryu_params
+    params.require(:senryu).permit(:kamigo, :nakashichi, :shimogo)
+  end
 end
