fa987b6a8dc6e65baa8ffb438f0f46188f3b8c8d
wakashi03 <wakachaso38@gmail.com>
2024-01-17 23:48:03 +0900

    Add routes for favorite actions

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,9 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
   resources :senryus do
     resources :comments, only: %i[create], shallow: true
+    collection do
+      get :favorites
+    end
   end
+  resources :favorites, only: %i[create destroy]
 end
