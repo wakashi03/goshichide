5d58c20d0da449e610aa4286af6130d37c2a3123
wakashi03 <wakachaso38@gmail.com>
2024-02-20 20:45:34 +0900

    Update themes for daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,38 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [require("daisyui")],
-  
-  daisyui: {
-    themes: ["garden"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bizud: ['BIZ UDGothic', 'sans-serif'],
+      },
+    },
   },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+          "primary": "#d1d5db",
+                  
+          "secondary": "#d1d5db",
+                  
+          "accent": "#d1d5db",
+                  
+          "neutral": "#d1d5db",
+                  
+          "base-100": "#ffffff",
+                  
+          "info": "#d1d5db",
+                  
+          "success": "#d1d5db",
+                  
+          "warning": "#d1d5db",
+                  
+          "error": "#d1d5db",
+                  },
+                },
+              ],
+            },
 }
