f2190eeda3530a612c9fc65fa0aaf41f8db59074
wakashi03 <wakachaso38@gmail.com>
2024-02-21 04:36:11 +0900

    Update ajax to turbo_stream

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,24 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
-    @comment.save
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment.senryu }
+        format.turbo_stream
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@comment, partial: "comments/form", locals: { comment: @comment }), status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
     @comment = current_user.comments.find(params[:id])
-    @comment.destroy!
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to @comment.senryu }
+      format.turbo_stream
+    end
   end
 
   private
