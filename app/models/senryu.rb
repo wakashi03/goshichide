d4adf54372d01b8364bddf3f654521d4b37f8879
wakashi03 <wakachaso38@gmail.com>
2024-04-09 11:06:25 +0900

    Remove senryu_ranking_service.rb

--- app/models/senryu.rb
+++ app/models/senryu.rb
@@ -13,4 +13,16 @@ class Senryu < ApplicationRecord
                   using: {
                     tsearch: { prefix: true }
                   }
+
+  # お気に入りの数に基づいて川柳をランキングするクラスメソッド
+  def self.ranked_by_favorites
+    all_senryus = where('favorites_count > 0').order(favorites_count: :desc)
+    rank = 0
+    ranked_senryus = all_senryus.chunk { |senryu| senryu.favorites_count }
+                                .flat_map do |favorites_count, senryus|
+                                  rank += 1
+                                  senryus.map { |senryu| [rank, senryu] }
+                                end
+    ranked_senryus.take_while { |rank, _senryu| rank <= 3 }
+  end
 end
